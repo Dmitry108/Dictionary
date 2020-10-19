f0a09d2e15ad2ff2197dee4912d296d3f671509c
Dmitrii Babanov <babanov1984@mail.ru>
2020-10-19 04:23:15 +0300

    fixed mainviewmodel

--- app/src/main/java/ru/dim/dictionary/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/dim/dictionary/viewmodel/MainViewModel.kt
@@ -1,13 +1,11 @@
 package ru.dim.dictionary.viewmodel
 
 import androidx.lifecycle.LiveData
-import io.reactivex.observers.DisposableObserver
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.dim.dictionary.interactor.MainInteractor
 import ru.dim.dictionary.model.ViewState
-import ru.dim.dictionary.model.entity.SearchResult
 
 class MainViewModel (
     private val interactor: MainInteractor
@@ -23,20 +21,6 @@ class MainViewModel (
         }
         return liveData
     }
-
-    private fun вытащитьДанныеИзТогоЧтоПридет(тоЧтоПришло: List<SearchResult>): ViewState {
-        return ViewState.Success(тоЧтоПришло)
-    }
-    private fun getObserver(): DisposableObserver<ViewState> =
-            object : DisposableObserver<ViewState>() {
-                override fun onNext(viewState: ViewState) {
-                    liveData.value = viewState
-                }
-                override fun onError(e: Throwable) {
-                    liveData.value = ViewState.Error(e)
-                }
-                override fun onComplete() { }
-            }
 
     override fun handleError(throwable: Throwable) {
         liveData.value = ViewState.Error(throwable)
